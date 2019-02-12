165971df4c75e620aabf7adf5e81572d9e347517
BlazinAmazin <kory.heinichen@gmail.com>
2019-02-12 09:31:08 -0800

    First Commit

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour {
+
+	public float speed;
+	public float jumpHeight;
+	public LayerMask ground;
+	public Transform feet;
+
+	private Vector3 direction;
+	private Rigidbody rb;
+
+	public float rotationSpeed = 1f;
+	private float minY = -60f;
+	private float maxY = 60f;
+	private float rotationY = 0f;
+	private float rotationX = 0f;
+    private bool isGrounded;
+
+	// Use this for initialization
+	void Start () {
+        isGrounded = false;
+        speed = 5.0f;
+		jumpHeight = 3.0f;
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		direction = Vector3.zero;
+
+		direction.z = Input.GetAxis("Horizontal");
+		direction.x = Input.GetAxis("Vertical");
+		direction = direction.normalized;
+
+		if(direction.x != 0)
+			rb.MovePosition(rb.position + transform.right * direction.x * speed * Time.deltaTime);
+		if(direction.z != 0)
+			rb.MovePosition(rb.position + transform.forward * direction.z * speed * Time.deltaTime);
+
+        /* Player rotation */
+        rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * rotationSpeed;
+        rotationY += Input.GetAxis("Mouse Y") * rotationSpeed;
+        rotationY = Mathf.Clamp(rotationY, minY, maxY);
+        transform.localEulerAngles = new Vector3(0f, rotationX, rotationY);
+
+        //isGrounded = Physics.CheckSphere(feet.position, 0.1f, ground, QueryTriggerInteraction.Ignore);
+		if(Input.GetButtonDown("Jump") /*&& isGrounded*/) {
+			rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+		}
+        
+	}
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    public GameObject Canvas;
+    public GameObject Camera;
+    bool Paused = false;
+
+    void Start()
+    {
+        Canvas.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Paused == true)
+            {
+                Time.timeScale = 1.0f;
+                Canvas.gameObject.SetActive(false);
+                Cursor.visible = false;
+                Screen.lockCursor = true;
+
+                Paused = false;
+            }
+            else
+            {
+                Time.timeScale = 0.0f;
+                Canvas.gameObject.SetActive(true);
+                Cursor.visible = true;
+                Screen.lockCursor = false;
+                Paused = true;
+            }
+        }
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+        Canvas.gameObject.SetActive(false);
+        Cursor.visible = false;
+        Screen.lockCursor = true;
+
+    }
+}
